cbd2fd61cc537f248d7e38a2f2efd061913e2376
Dongge Tan <donggetan@Dongges-MacBook-Pro.local>
2024-10-30 14:29:39 +0800

    fix minor

--- golang/quickstart.go
+++ golang/quickstart.go
@@ -3,15 +3,13 @@ package main
 func include(source string, sub string) bool {
 	var i, j int
 	for i = 0; i < len(source); i++ {
-		if source[i] == sub[0] {
-			for j = 0; j < len(sub); j++ {
-				if source[i+j] != sub[j] {
-					break
-				}
+		for j = 0; j < len(sub); j++ {
+			if source[i+j] != sub[j] {
+				break
+			}
 
-				if j == len(sub)-1 {
-					return true
-				}
+			if j == len(sub)-1 {
+				return true
 			}
 		}
 
