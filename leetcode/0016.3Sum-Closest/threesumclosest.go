53491844412ff340eedf70caa801ab59e7eff5fb
Dongge Tan <donggetan@Dongges-MacBook-Pro.local>
2024-11-13 11:22:16 +0800

    fix

--- leetcode/0016.3Sum-Closest/threesumclosest.go
+++ leetcode/0016.3Sum-Closest/threesumclosest.go
@@ -5,20 +5,14 @@ import (
 )
 
 func ThreeSumClosestBruteForce(nums []int, target int) int {
-	var minDiff int
-	var init bool = true
+	var minDiff int = math.MaxInt
 	for i := 0; i < len(nums)-2; i++ {
 		for j := i + 1; j < len(nums)-1; j++ {
 			for k := j + 1; k < len(nums); k++ {
 				sum := nums[i] + nums[j] + nums[k]
 				diff := sum - target
-				if init {
+				if math.Abs(float64(diff)) < math.Abs(float64(minDiff)) {
 					minDiff = diff
-					init = false
-				} else {
-					if math.Abs(float64(diff)) < math.Abs(float64(minDiff)) {
-						minDiff = diff
-					}
 				}
 			}
 		}
