064b29f90cbdb7d23ae9e84e29a4eb93e180a925
Dongge Tan <donggetan@Dongges-MacBook-Pro.local>
2025-06-11 10:32:08 +0800

    interleaving in dp

--- leetcode/0097.Interleaving-String/interleavingstring.go
+++ leetcode/0097.Interleaving-String/interleavingstring.go
@@ -44,3 +44,30 @@ func IsInterleave(s1 string, s2 string, s3 string) bool {
 
 	return interleave(0, 0, 0, true) || interleave(0, 0, 0, false)
 }
+
+func IsInterleaveDP(s1 string, s2 string, s3 string) bool {
+	if len(s1)+len(s2) != len(s3) {
+		return false
+	}
+
+	dp := make([][]bool, len(s1)+1)
+	for i := 0; i < len(s1)+1; i++ {
+		dp[i] = make([]bool, len(s2)+1)
+	}
+
+	for i := 0; i <= len(s1); i++ {
+		for j := 0; j <= len(s2); j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = true
+			} else if i == 0 {
+				dp[i][j] = dp[i][j-1] && s2[j-1] == s3[i+j-1]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i+j-1]
+			} else {
+				dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) || (dp[i][j-1] && s2[j-1] == s3[i+j-1])
+			}
+		}
+	}
+
+	return dp[len(s1)][len(s2)]
+}
