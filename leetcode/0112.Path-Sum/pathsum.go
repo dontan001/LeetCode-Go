8b80e2998bb6124fa0d2da8bda7ce7eeb81597c4
Dongge Tan <donggetan@Dongges-MacBook-Pro.local>
2025-06-25 10:43:35 +0800

    path sum

---
+++ leetcode/0112.Path-Sum/pathsum.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func HasPathSum(root *TreeNode, targetSum int) bool {
+
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+
+	return HasPathSum(root.Left, targetSum-root.Val) ||
+		HasPathSum(root.Right, targetSum-root.Val)
+}
